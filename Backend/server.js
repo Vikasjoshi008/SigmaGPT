1742844d1bcac9d5393207946d438d9e23acce6b
Vikas Joshi <vikasjoshi10889@gmail.com>
2025-11-01 23:23:39 +0530

    fixed error3

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const PORT = 8080;
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://sigma-gpt-livid.vercel.app", " http://localhost:5173"],
+  origin: ["https://sigma-gpt-livid.vercel.app"],
   credentials: true,
 }));
 
