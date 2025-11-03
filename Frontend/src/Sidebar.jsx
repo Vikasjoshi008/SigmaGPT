186c871293bc521ef8ad3d883b3d3a533d842656
Vikas Joshi <vikasjoshi10889@gmail.com>
2025-11-03 20:01:46 +0530

    fixed error final4

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,6 +1,6 @@
 import "./Sidebar.css";
 import { MyContext } from "./MyContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 
