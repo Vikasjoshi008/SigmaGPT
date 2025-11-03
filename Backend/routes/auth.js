8afa0ac4c303fd42b0891fd6711afbe7c637320e
Vikas Joshi <vikasjoshi10889@gmail.com>
2025-11-03 12:08:27 +0530

    fixed error 5

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -42,6 +42,11 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 //   }
 // });
 
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"], prompt: "select_account" })
+);
+
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: 'https://sigma-gpt.vercel.app/login' }),
   (req, res) => {
